9a1ac04b8a482876afdd6a2ca9d84b39a835995a
Felipe Arias <felipe.arias.t@ing.uchile.cl>
2021-04-20 23:52:55 -0400

    desafio

--- nombre_app/urls.py
+++ nombre_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns =[
     path('', views.index, name="index"),
+    path('formulario/', views.formulario, name="formulario"),
+    path('resultados/', views.resultados, name="resultados"),
+
 ]
--- nombre_app/views.py
+++ nombre_app/views.py
@@ -4,4 +4,34 @@ from django.shortcuts import render
 def index(request):
     return render(request,'nombre_app/index.html')
 
+def formulario(request):
+    return render(request, 'nombre_app/formulario.html')
 
+def resultados(request):
+
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    age = request.POST['age']
+    email = request.POST['email']
+    dieta = request.POST['dieta']
+    vivienda = request.POST['vivienda']
+
+    animales = []
+    if 'animal1' in request.POST:  # OJO con los checkboxes
+        animales.append(request.POST['animal1'])
+    if 'animal2' in request.POST:  # OJO con los checkboxes
+        animales.append(request.POST['animal2'])
+
+
+    context = {}
+    context['fname'] = fname
+    context['lname'] = lname
+    context['age'] = age
+    context['email'] = email
+    context['dieta'] = dieta
+    context['vivienda'] = vivienda
+    context['animales'] = animales
+
+
+
+    return render(request, 'nombre_app/resultados.html',context)
